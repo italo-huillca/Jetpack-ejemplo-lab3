988c2d05bd6d041bc8a8580c77b9b18edd8ce919
Italo H <huillca.italo@gmail.com>
2025-04-08 12:09:40 -0500

    Añadiendo una vista previa con mi nombre

--- app/src/main/java/com/example/bienvenidoalcurso/MainActivity.kt
+++ app/src/main/java/com/example/bienvenidoalcurso/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     BienvenidoAlCursoTheme {
-        Greeting("Android")
+        Greeting("Italo Huillca")
     }
 }
